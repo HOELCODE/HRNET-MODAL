036e54a1c5340567226f43cb59aa4b839698fc57
HOELCODE <hcostantini1@gmail.com>
2025-05-14 20:22:43 +0200

    modifications code background

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -12,22 +12,38 @@ const closeModal = () => {
 }
 
 
-
-
 const Modal = () => {
     return (
         <>
-            <div className="modal">
-                <div className='cross-container' onClick={closeModal}>
-                    <span className="close">&times;</span>
-                </div>
-                <div className="modal-content">
-                    <h2>Employee Created!</h2>
+            <div className="modal-container">
+                <div className="modal">
+                    <div className='cross-container' onClick={closeModal}>
+                        <span className="close">&times;</span>
+                    </div>
+                    <div className="modal-content">
+                        <h2>Employee Created!</h2>
+                    </div>
                 </div>
             </div>
 
             <style>
                 {`
+
+                    .modal-container {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                    background-color: rgba(0, 0, 0, 0.5);
+                    z-index: 999;
+                    filter: blur(2px);
+                    transition: filter 0.3s ease;
+                    }
+
                     .modal {
                     box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.5);
                     border-radius: 10px;
@@ -36,6 +52,7 @@ const Modal = () => {
                     width: 154px;
                     display: none;
                     background-color: #fff;
+                    z-index: 1000;
                     }
 
                     .modal-content {
